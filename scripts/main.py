a128d3bfbfaf9cdc421cc73c60f102badbbea7b7
Rafiqi Raihan <ravickyraihan@gmail.com>
2025-07-21 12:56:06 +0700

    update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -3,17 +3,21 @@ from joblib import dump
 from automation_preprocessing import TextPreprocessor
 
 def preprocess_text_data(input_path, output_path_data, output_path_pipeline):
-    
     df = pd.read_csv(input_path)
+
+    df.columns = df.columns.str.strip()
+
+    if 'review_text' not in df.columns:
+        raise KeyError(f"Kolom 'review_text' tidak ditemukan dalam file: {input_path}\nKolom tersedia: {df.columns.tolist()}")
 
     text_preprocessor = TextPreprocessor()
     df['processed_text'] = text_preprocessor.fit_transform(df['review_text'])
 
     df.to_csv(output_path_data, index=False)
-    print(f"Data teks telah diproses dan disimpan ke:{output_path_data}")
+    print(f"Data teks telah diproses dan disimpan ke: {output_path_data}")
 
     dump(text_preprocessor, output_path_pipeline)
-    print(f"TextPrerocessor disimpan ke:{output_path_pipeline}")
+    print(f"TextPreprocessor disimpan ke: {output_path_pipeline}")
 
 if __name__ == "__main__":
     preprocess_text_data(
